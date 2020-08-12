5967bcc98133e41e9c58008d70eff1d0571af0c0
saminsoolee <samuelminsoolee71@gmail.com>
2020-08-12 22:22:33 +0000

    Rendering Grades - Sam Lee

--- user-can-view-all-grades/main.js
+++ user-can-view-all-grades/main.js
@@ -6,6 +6,7 @@
 //     this.handleStart = handleStart;
 //   }
 // }
-
-  var app = new App()
+  var table = document.querySelector('table');
+  var gradeTable = new GradeTable(table)
+  var app = new App(gradeTable)
   app.start()
